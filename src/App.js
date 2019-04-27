439b741fb55837476bf16f93921ce881c49c8a26
unknown <ashay11raj@gmail.com>
2019-04-27 13:08:16 +0530

    backend integration

--- src/App.js
+++ src/App.js
@@ -5,33 +5,51 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       columnDefs: [{
-        headerName: "Make", field: "make"
+        headerName: "Book Id", field: "bookId"
       }, {
-        headerName: "Model", field: "model"
+        headerName: "Book Name", field: "bookName"
       }, {
-        headerName: "Price", field: "price"
+        headerName: "Author", field: "author"
+      }, {
+        headerName: "Published On", field: "dateOfPublication"
       }],
       rowData: [{
-        make: "Toyota", model: "Celica", price: 35000
-      }, {
-        make: "Ford", model: "Mondeo", price: 32000
-      }, {
-        make: "Porsche", model: "Boxter", price: 72000
+        bookId: 1, bookName: "Celica", author: "Author 1", dateOfPublication: "2019-02-19"
       }]
     }
   }
-
+  componentDidMount(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('accept', 'application/json');
+    let option = {
+      method: "GET",
+      mode: "no-cors",
+      credentials: "include",
+      headers: headers
+    };
+    let url = 'http://localhost:8080/bookstore/getbooks/bookName'; 
+    fetch(url, option)
+     .then(result => result)
+     .then(rowData => console.log("rowData" + rowData))
+     .catch(error => console.error('Error:', error));
+      //.then(rowData => this.setState({rowData}));
+  }
   render() {
     return (
       <div 
-        className="App"
+      className="app ag-theme-balham"
+      style={{ 
+        height: '500px', 
+        width: '800px' }} 
       >
-        <AgGridReact
+        <AgGridReact 
           columnDefs={this.state.columnDefs}
           rowData={this.state.rowData}>
         </AgGridReact>
